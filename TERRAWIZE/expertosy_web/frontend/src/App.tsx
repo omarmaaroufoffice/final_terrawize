f09a9c51e8aa43cd867330f3d7d1de14d532df7c
omarmaaroufoffice <omar.maarouf.office@gmail.com>
2025-01-02 18:32:54 -0500

    feat: implement universal star background across all pages

--- TERRAWIZE/expertosy_web/frontend/src/App.tsx
+++ TERRAWIZE/expertosy_web/frontend/src/App.tsx
@@ -1,40 +1,24 @@
 import React from 'react';
-import { 
-  createBrowserRouter, 
-  RouterProvider, 
-  createRoutesFromElements, 
-  Route, 
-  Outlet
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import QuestionnairePage from './components/QuestionnairePage';
 import ResultsPage from './components/ResultsPage';
-import Logo from './components/shared/Logo';
+import StarBackground from './components/shared/StarBackground';
 import './App.css';
 
-const AppLayout: React.FC = () => {
-  return (
-    <div className="app">
-      <div className="content">
-        <Logo />
-        <Outlet />
-      </div>
-    </div>
-  );
-};
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<AppLayout />}>
-      <Route index element={<LandingPage />} />
-      <Route path="questionnaire" element={<QuestionnairePage />} />
-      <Route path="results" element={<ResultsPage />} />
-    </Route>
-  )
-);
-
 const App: React.FC = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <Router>
+      <div className="app">
+        <StarBackground />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/questionnaire" element={<QuestionnairePage />} />
+          <Route path="/results" element={<ResultsPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
