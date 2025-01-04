4b3af862b57d4d6ae3461b345acf445ab9d89291
Omar Maarouf <omarmaarouf@MacBookPro.lan>
2025-01-04 04:24:50 -0500

    feat: enhance ResultsPage color scheme with improved gradients and accessibility

--- TERRAWIZE/expertosy_web/frontend/src/components/shared/Logo.tsx
+++ TERRAWIZE/expertosy_web/frontend/src/components/shared/Logo.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Logo.css';
 
 const Logo: React.FC = () => {
   return (
@@ -13,8 +14,7 @@ const Logo: React.FC = () => {
         </div>
       </div>
       <div className="logo-core">
-        <div className="logo-inner-circle"></div>
-        <div className="logo-pulse"></div>
+        <img src="/static/media/logo.2e2c642bf5931adb622080029bad" alt="Expertosy Logo" />
       </div>
       <div className="logo-rings"></div>
       <span className="nav-logo-text">Expertosy</span>
