64a5e2e5424752053f19ec4f53340d9f6ac699b9
Omar Maarouf <omarmaarouf@MacBookPro.lan>
2025-01-04 03:43:59 -0500

    feat: enhance logo with mesmerizing rotating animations

--- TERRAWIZE/expertosy_web/frontend/src/components/shared/Logo.tsx
+++ TERRAWIZE/expertosy_web/frontend/src/components/shared/Logo.tsx
@@ -6,9 +6,16 @@ const Logo: React.FC = () => {
   return (
     <Link to="/" className="logo">
       <div className="logo-icon">
-        <div className="logo-particles"></div>
+        <div className="logo-outer-ring"></div>
+        <div className="logo-middle-ring"></div>
         <div className="logo-inner-ring"></div>
-        <img src="/logo.png" alt="Expertosy Logo" />
+        <div className="logo-particles">
+          <div className="particle particle-1"></div>
+          <div className="particle particle-2"></div>
+          <div className="particle particle-3"></div>
+        </div>
+        <div className="logo-glow"></div>
+        <img src="/logo.png" alt="Expertosy Logo" className="logo-image" />
       </div>
       <span className="logo-text">Expertosy</span>
     </Link>
